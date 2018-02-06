b702126bc0115c0e9e83b7f35aaa27a904a56d06
MannieJay <jhall02258@gmail.com>
2018-02-06 17:27:30 -0500

    ex3 finished

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -7,52 +7,81 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *ReadFile(char *filename)
+// char *ReadFile(char *filename)
+// {
+//   char *buffer = NULL;
+//   int string_size, read_size;
+//   FILE *handler = fopen(filename, "r");
+
+//   if (handler)
+//   {
+//     // Seek the last byte of the file
+//     fseek(handler, 0, SEEK_END);
+//     // Offset from the first to the last byte, or in other words, filesize
+//     string_size = ftell(handler);
+//     // go back to the start of the file
+//     rewind(handler);
+
+//     // Allocate a string that can hold it all
+//     buffer = (char *)malloc(sizeof(char) * (string_size + 1));
+
+//     // Read it all in one operation
+//     read_size = fread(buffer, sizeof(char), string_size, handler);
+
+//     // fread doesn't set it so put a \0 in the last position
+//     // and buffer is now officially a string
+//     buffer[string_size] = '\0';
+
+//     if (string_size != read_size)
+//     {
+//       // Something went wrong, throw away the memory and set
+//       // the buffer to NULL
+//       free(buffer);
+//       buffer = NULL;
+//     }
+
+//     // Always remember to close the file.
+//     fclose(handler);
+//   }
+
+//   return buffer;
+// }
+
+// int main()
+// {
+//   int rc;
+//   char *string = ReadFile("text.txt");
+//   rc = fork();
+
+//   if (rc < 0)
+//   {
+//     fprintf(stderr, "fork failed\n");
+//     exit(1);
+//   }
+//   else if (rc == 0)
+//   {
+//     printf("hello, child here (pid: %d) and textfile string: %s\n", (int)getpid(), string);
+//   }
+//   else
+//   {
+//     printf("hello, parent here (pid: %d) of child %d and textfile string: %s\n", (int)getpid(), rc, string);
+//   }
+
+//   if (string)
+//   {
+//     puts(string);
+//     free(string);
+//   }
+
+//   return 0;
+// }
+
+int main(int argc, char *argv[])
 {
-  char *buffer = NULL;
-  int string_size, read_size;
-  FILE *handler = fopen(filename, "r");
+  FILE *fp;
+  fp = fopen("text.txt", "w");
 
-  if (handler)
-  {
-    // Seek the last byte of the file
-    fseek(handler, 0, SEEK_END);
-    // Offset from the first to the last byte, or in other words, filesize
-    string_size = ftell(handler);
-    // go back to the start of the file
-    rewind(handler);
-
-    // Allocate a string that can hold it all
-    buffer = (char *)malloc(sizeof(char) * (string_size + 1));
-
-    // Read it all in one operation
-    read_size = fread(buffer, sizeof(char), string_size, handler);
-
-    // fread doesn't set it so put a \0 in the last position
-    // and buffer is now officially a string
-    buffer[string_size] = '\0';
-
-    if (string_size != read_size)
-    {
-      // Something went wrong, throw away the memory and set
-      // the buffer to NULL
-      free(buffer);
-      buffer = NULL;
-    }
-
-    // Always remember to close the file.
-    fclose(handler);
-  }
-
-  return buffer;
-}
-
-int main()
-{
-  int rc;
-  char *string = ReadFile("text.txt");
-  rc = fork();
-
+  int rc = fork();
   if (rc < 0)
   {
     fprintf(stderr, "fork failed\n");
@@ -60,18 +89,16 @@ int main()
   }
   else if (rc == 0)
   {
-    printf("hello, child here (pid: %d) and textfile string: %s\n", (int)getpid(), string);
+    printf("child process\n");
+    char *child_str = "This is a child string!\n";
+    fwrite(child_str, 1, strlen(child_str), fp);
   }
   else
   {
-    printf("hello, parent here (pid: %d) of child %d and textfile string: %s\n", (int)getpid(), rc, string);
+    printf("parent process\n");
+    char *parent_str = "This is parent string!\n";
+    fwrite(parent_str, 1, strlen(parent_str), fp);
   }
-
-  if (string)
-  {
-    puts(string);
-    free(string);
-  }
-
+  fclose(fp);
   return 0;
 }
--- ex3/ex3.c
+++ ex3/ex3.c
@@ -1,14 +1,27 @@
-// Write another program using `fork()`. The child process should print "hello";
-// the parent process should print "goodbye". You should ensure that the child
-// process always prints first.
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[])
+int main()
 {
-    // Your code here
+    int status;
+    int rc = fork();
 
-    return 0;
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("Helloe!\n");
+    }
+    else
+    {
+        while (waitpid(rc, &status, 0) == 0)
+        {
+            sleep(1);
+        }
+        printf("Goodbye!\n");
+    }
 }
